63cca1086122b9a9216e0f631c8fb97d376d1318
Justin_Jung <jungdw0624@gmail.com>
2019-06-27 00:20:10 +0900

    material ui update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React,{ Component } from 'react';
 import Customer from './components/Customer';
+import './App.css';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit*3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -25,18 +44,27 @@ const customers = [
 
 class App extends Component{
   render(){
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(temp => {
-            return (
-              <Customer key={temp.id} id={temp.id} image={temp.image} name={temp.name} gender={temp.gender} />
-            )
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>성별</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(temp => { 
+              return (<Customer key={temp.id} id={temp.id} image={temp.image} name={temp.name} gender={temp.gender} />)
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
